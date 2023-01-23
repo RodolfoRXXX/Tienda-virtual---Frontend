8e1b92389610271848ae1813ba8ee80f5b32668c
Rodolfo <rodolfo.singular@gmail.com>
2023-01-23 09:35:59 -0300

    instalación de angular con problemas, creo componente header

---
+++ store/src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+    exports: [MatToolbarModule]  //aquí nombramos los módulos de material que queremos importar
+})
+
+export class MaterialModule {
+
+}
