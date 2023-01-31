f2633009246c9b4f309f2821c304b381f61efd1b
Rodolfo <rodolfo.singular@gmail.com>
2023-01-31 19:00:37 -0300

    Creación de la carpeta pages y el módulo separado products

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+    {
+        path: "**", redirectTo: "", pathMatch: "full", //Esta ruta siempre va al final de todo, porque primero debe comprobar las sí existentes
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
